66740d0024cbe8e702b6b049d9471e99388ea7f6
dpazme <115536451+dpazme@users.noreply.github.com>
2023-03-19 23:05:59 +0100

    dam01dianapaz

---
+++ src/dni.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+/**
+ * @author DIANA PAZ
+ */
+
+public class dni{
+
+    static Scanner sc = new Scanner(System.in);
+    /**
+     * El método main recibe la entrada del usuario y llama al método obtenerLetra para calcular la letra del DNI.
+     * Finalmente, imprime el número del DNI y su letra correspondiente.
+     *
+     * @param args argumentos de línea de comando
+     */
+
+    public static void main(String[] args){
+        System.out.println("Este algoritmo calcula la letra del DNI");
+        System.out.println("Introduce tu dni");
+       int dni = sc.nextInt();
+        char letras = obtenerLetraAleatoria(dni);
+        System.out.println("Tu dni es "+ dni +letras);
+        System.out.println("ESTE ES TU NUMERO DE DNI ALEATORIO");
+
+
+
+    }
+
+    /**
+     * El método obtenerLetra calcula la letra del DNI a partir del número del DNI recibido como parámetro.
+     *
+     * @param dni número del DNI
+     * @return letra correspondiente al número del DNI
+     */
+
+    public static char obtenerLetraAleatoria(int dni){
+        char[] letraAleatoria = {'T', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        int resto = dni % 23;
+        return letraAleatoria[resto];
+    }
+}
